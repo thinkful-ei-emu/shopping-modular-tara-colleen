300b7e4900ccd0ceff4cd6f32fdb0e0bf5917e0c
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-10 11:19:12 -0500

    moved CRUD functions from shoppingList into store.js

--- scripts/store.js
+++ scripts/store.js
@@ -11,10 +11,53 @@ const store = (function() {
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+  
+  function findById(id) {
+    return items.find(item => id===item.id);
+  }
+
+  function addItem(name) {
+    try {
+      console.log(Item.validateName(name));
+      this.items.push(Item.create(name));
+      shoppingList.render();
+    }
+    catch(error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
+  }
+
+  function findAndToggleChecked(id) {
+    const item = this.findById(id);
+    item.checked = !item.checked;
+  }
+
+  function findAndUpdateName(id, newName) {
+    const item = this.findById(id);
+    try {
+      Item.validateName(newName);
+      item.name = newName;
+      render();
+    }
+    catch(error) {
+      console.log(`Cannot change item: ${error.message}`);
+    }
+  }
+
+  function findAndDelete(id) {
+    const item = this.findById(id);
+    index = item.indexOf(item);
+    store.items.splice(index, 1);
+  }
 
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   }
 }());
